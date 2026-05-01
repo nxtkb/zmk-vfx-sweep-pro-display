091482f74d579a00c12c1435f2cc366717befe0d
lynnlee0522 <lilin@arcblock.io>
2026-05-01 13:26:35 +0800

    Use custom Sweep Pro display widgets

--- boards/shields/sweep_display/custom_status_screen.c
+++ boards/shields/sweep_display/custom_status_screen.c
@@ -21,27 +21,27 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 LV_FONT_DECLARE(lv_custom_symbol);
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
 static struct zmk_widget_battery_status battery_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_OUTPUT_STATUS)
 static struct zmk_widget_output_status output_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_PERIPHERAL_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
 static struct zmk_widget_peripheral_status peripheral_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
 static struct zmk_widget_layer_status layer_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_WPM_STATUS)
 static struct zmk_widget_wpm_status wpm_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_INDICATORS_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_INDICATORS_STATUS)
 static struct zmk_widget_hid_indicators_status indicators_status_widget;
 #endif
 
@@ -77,7 +77,7 @@ lv_obj_t *zmk_display_status_screen() {
 
   set_epaper_screen_style(screen);
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_OUTPUT_STATUS)
   zmk_widget_output_status_init(&output_status_widget, screen);
   set_epaper_widget_style(zmk_widget_output_status_obj(&output_status_widget));
   lv_obj_set_style_text_font(zmk_widget_output_status_obj(&output_status_widget),
@@ -86,7 +86,7 @@ lv_obj_t *zmk_display_status_screen() {
                LV_ALIGN_TOP_LEFT, 0, 0);
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
   zmk_widget_battery_status_init(&battery_status_widget, screen);
   set_epaper_widget_style(
       zmk_widget_battery_status_obj(&battery_status_widget));
@@ -98,7 +98,7 @@ lv_obj_t *zmk_display_status_screen() {
   set_epaper_widget_style(zmk_widget_logo_obj(&logo_widget));
   lv_obj_align(zmk_widget_logo_obj(&logo_widget), LV_ALIGN_TOP_MID, 0, 34);
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_PERIPHERAL_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
   zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
   set_epaper_widget_style(
       zmk_widget_peripheral_status_obj(&peripheral_status_widget));
@@ -114,21 +114,21 @@ lv_obj_t *zmk_display_status_screen() {
   lv_obj_align(zmk_widget_kbd_name_obj(&kbd_name_widget), LV_ALIGN_TOP_MID, 0,
                94);
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
   zmk_widget_layer_status_init(&layer_status_widget, screen);
   set_epaper_widget_style(zmk_widget_layer_status_obj(&layer_status_widget));
   lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget),
                LV_ALIGN_TOP_MID, 0, 116);
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_WPM_STATUS)
   zmk_widget_wpm_status_init(&wpm_status_widget, screen);
   set_epaper_widget_style(zmk_widget_wpm_status_obj(&wpm_status_widget));
   lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget),
                LV_ALIGN_BOTTOM_LEFT, 0, 0);
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_INDICATORS_STATUS)
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_INDICATORS_STATUS)
   zmk_widget_hid_indicators_status_init(&indicators_status_widget, screen);
   set_epaper_widget_style(
       zmk_widget_hid_indicators_status_obj(&indicators_status_widget));
